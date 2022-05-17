5839a3f489d44f6d5f0de7d9040aba8828401e1a
Shihao <dongshihao1019@gmail.com>
2022-05-17 21:11:01 +0200

    It can compile roughly.

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import cv2
+import mediapipe as mp
+import numpy as np
 
 root = Tk()
 
@@ -9,7 +12,100 @@ root.title('Human Pose estimate')
 root.maxsize(1920,1080)
 root.minsize(400,270)
 
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle
+    
+def show():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+    cap = cv2.VideoCapture(0)
 
+    # Curl counter variables
+    counter = 0 
+    stage = None
+
+    ## Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+          
+            # Make detection
+            results = pose.process(image)
+        
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+                
+                # Get coordinates
+                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                
+                # Calculate angle
+                angle = calculate_angle(shoulder, elbow, wrist)
+                
+                # Visualize angle
+                cv2.putText(image, str(angle), 
+                               tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                
+                # Curl counter logic
+                if angle > 160:
+                    stage = "down"
+                if angle < 30 and stage =='down':
+                    stage="up"
+                    counter +=1
+                    print(counter)
+                           
+            except:
+                pass
+            
+            # Render curl counter
+            # Setup status box
+            cv2.rectangle(image, (415,407), (640,480), (245,117,16), -1)
+            
+            # Rep data
+            cv2.putText(image, 'REPS', (415,460), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), 
+                        (540,460), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(255, 245,117), thickness=2, circle_radius=2) 
+                                     )               
+            
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+##########################################################################################################################
 # create a canvas
 canvas = Canvas(root, width=500, height=300, bg=None)
 image_file = PhotoImage(file="2.gif")
@@ -25,12 +121,18 @@ myLabel = Label(root, text='Human pose estimate',
 
 myLabel.pack()
 
-myButton = Button(root,text='hit me',     
+myButton1 = Button(root,text='start',     
     width=15, height=2, 
-    #command=hit_me
+    command=show
     )
    
+myButton1.pack()
+
+myButton2 = Button(root,text='stop',     
+    width=15, height=2, 
+    command=quit
+    )
    
-myButton.pack()
+myButton2.pack()
 
 root.mainloop()
